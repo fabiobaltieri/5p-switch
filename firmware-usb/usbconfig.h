e628005c949c1297b49bb38544af7f5362bdf981
Fabio Baltieri <fabio.baltieri@gmail.com>
2012-05-15 11:34:12 +0200

    fw-usb: fix usb descriptors

--- firmware-usb/usbconfig.h
+++ firmware-usb/usbconfig.h
@@ -119,7 +119,7 @@ section at the end of this file).
 /* Define this to 1 if the device has its own power supply. Set it to 0 if the
  * device is powered from the USB bus.
  */
-#define USB_CFG_MAX_BUS_POWER           100
+#define USB_CFG_MAX_BUS_POWER           500
 /* Set this variable to the maximum USB bus power consumption of your device.
  * The value is in milliamperes. [It will be divided by two since USB
  * communicates power requirements in units of 2 mA.]
@@ -252,8 +252,8 @@ section at the end of this file).
  * obdev's free shared VID/PID pair. See the file USB-IDs-for-free.txt for
  * details.
  */
-#define USB_CFG_DEVICE_NAME     'T', 'e', 's', 't', ' ', 'D', 'e', 'v', 'i', 'c', 'e'
-#define USB_CFG_DEVICE_NAME_LEN 11
+#define USB_CFG_DEVICE_NAME     '5', 'p', '-', 's', 'w', 'i', 't', 'c', 'h'
+#define USB_CFG_DEVICE_NAME_LEN 9
 /* Same as above for the device name. If you don't want a device name, undefine
  * the macros. See the file USB-IDs-for-free.txt before you assign a name if
  * you use a shared VID/PID.
